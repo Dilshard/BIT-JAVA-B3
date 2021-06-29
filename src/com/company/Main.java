08d021628a97750770e7e1fa4d37425f06b4a0a9
Dilshard <mrdilshard@gmail.com>
2021-06-29 17:32:13 +0530

    NIC Project Step 01

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+        System.out.print("Please enter your NIC number? ");
+        String NIC = read.nextLine();
+        String selectedYear = NIC.substring(0,2);
+        String selectedDayMonth = NIC.substring(2,5);
+
+        System.out.println("Year :"+selectedYear);
+        System.out.println("Month & Day :"+selectedDayMonth);
+
+    }
+}
